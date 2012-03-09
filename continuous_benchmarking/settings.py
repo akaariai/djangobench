c6aa44b423ac004a0827f87bd1f0d97d0deabe57
Anssi Kääriäinen <anssi.kaariainen@thl.fi>
2012-03-09 23:19:14 +0200

    Improved comments, added README and an example.html

--- continuous_benchmarking/settings.py
+++ continuous_benchmarking/settings.py
@@ -1,8 +1,10 @@
 GIT_DIR = '/home/akaariai/Programming/djbench/'
 DJANGOBENCH = '/home/akaariai/Programming/djangobench/djangobench/main.py'
 RESULTS_DIR = '/home/akaariai/tmp/'
-# A list of test to run: (djbench test name, first git commit to use for
-# running, dict of parameters to pass to djangobench)
+# A list of test to run:
+#   - djangobench test name
+#   - earliest git commit to include in the benchmark
+#   - dict of parameters to pass to djangobench)
 TESTS = [
     ('url_resolve', 'c2c622dc045c025e44417a32371991b436118861', {'-t': '30'}),
     ('query_all', 'c2c622dc045c025e44417a32371991b436118861', {'-t': '20'})
