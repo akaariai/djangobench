525826f03263b8d49213b1be42e6b0bc3dc31a81
Anssi Kääriäinen <anssi.kaariainen@thl.fi>
2012-03-09 22:53:02 +0200

    Somewhat sane defaults for the two existing tests...

--- continuous_benchmarking/settings.py
+++ continuous_benchmarking/settings.py
@@ -4,6 +4,6 @@ RESULTS_DIR = '/home/akaariai/tmp/'
 # A list of test to run: (djbench test name, first git commit to use for
 # running, dict of parameters to pass to djangobench)
 TESTS = [
-    ('url_resolve', 'a91fb321d8828d71975426466e497f1c6e3c9b71', {'-t': '30'}),
-    #('query_all', 'a91fb321d8828d71975426466e497f1c6e3c9b71', {'-t': '20'})
+    ('url_resolve', 'c2c622dc045c025e44417a32371991b436118861', {'-t': '30'}),
+    ('query_all', 'c2c622dc045c025e44417a32371991b436118861', {'-t': '20'})
 ]
